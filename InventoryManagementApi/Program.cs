eeaaf5e36b1702de3278a12929bc6808a30d59e6
Sérgio Alencar <sergiofalencar@gmail.com>
2025-12-28 12:17:19 -0300

    corrige Program.cs

--- InventoryManagementApi/Program.cs
+++ InventoryManagementApi/Program.cs
@@ -72,11 +72,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// using (var scope = app.Services.CreateScope())
-// {
-//     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-// }
-
 app.UseMiddleware<InventoryManagementApi.Middleware.ExceptionMiddleware>();
 
 app.UseSwagger();
