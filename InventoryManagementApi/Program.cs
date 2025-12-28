ea7bd343b52156f2fd33a25543515863ece064fb
Sérgio Alencar <sergiofalencar@gmail.com>
2025-12-28 11:30:50 -0300

    corrige Program.cs

--- InventoryManagementApi/Program.cs
+++ InventoryManagementApi/Program.cs
@@ -72,6 +72,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureDeleted();
     db.Database.EnsureCreated();
 }
 
