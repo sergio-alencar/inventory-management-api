ff6417c3d3d8d47bb0414dc57355017fdd94d20a
Sérgio Alencar <sergiofalencar@gmail.com>
2025-12-28 09:34:46 -0300

    prepara para deploy

--- InventoryManagementApi/Program.cs
+++ InventoryManagementApi/Program.cs
@@ -54,6 +54,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseMiddleware<InventoryManagementApi.Middleware.ExceptionMiddleware>();
 
 app.UseSwagger();
