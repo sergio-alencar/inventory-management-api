7c98383579ffd9f88c4ef5382455364d3ebb3814
Sérgio Alencar <sergiofalencar@gmail.com>
2025-12-28 11:38:13 -0300

    corrige Program.cs

--- InventoryManagementApi/Models/Product.cs
+++ InventoryManagementApi/Models/Product.cs
@@ -1,16 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace InventoryManagementApi.Models
 {
     public class Product
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; } = string.Empty;
+
         public string? Description { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
         public int Quantity { get; set; }
+
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
     }
 }
